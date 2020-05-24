f14b46f66c54e09aea7ad0b985db27d978a6699a
blamedcloud <denverwoodward@yahoo.com>
2020-05-24 12:59:36 -0400

    renamed scripts folder to bin

--- setup_bin.sh
+++ setup_bin.sh
@@ -3,8 +3,8 @@
 [ ! -d "$HOME/bin" ] && mkdir $HOME/bin
 
 thisDir=$(pwd)
-for script in $(ls scripts/)
+for script in $(ls bin/)
 do
     [ -f $HOME/bin/$script ] && rm $HOME/bin/$script
-    ln -s $thisDir/scripts/$script $HOME/bin/$script
+    ln -s $thisDir/bin/$script $HOME/bin/$script
 done
